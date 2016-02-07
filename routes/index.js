4ea10d676ac868c720c1e74dc4c453815b1436a6
bhuber2010 <bhuber@alumni.colostate.edu>
2016-02-06 17:39:32 -0800

    added test route to be protected

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res){
   res.json({ title: 'Express' });
 });
+
+router.use('/test' function(req, res){
+  res.json({signedIn: 'yes'});
+})
 
 module.exports = router;
