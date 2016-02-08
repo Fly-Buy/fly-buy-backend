8ccf9463583ab7945e0636047cec722573fa7746
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-08 08:27:10 -0700

    get route for airlines in place; returns json results

--- routes/airlines.js
+++ routes/airlines.js
@@ -1,3 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+
+
+router.get('/', function(req, res){
+  knex('airlines').then(function(airlines){
+    res.json(airlines);
+  })
+})
+
+
+module.exports = router;
