61dc41647fcab9cf30da7e293dc2f424b436177a
bhuber2010 <bhuber@alumni.colostate.edu>
2016-02-08 09:41:34 -0800

    added secure true to pgSession

--- app.js
+++ app.js
@@ -44,7 +44,10 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESS_SECRET,
   resave: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    secure: true
+  }
 }));
 
 
