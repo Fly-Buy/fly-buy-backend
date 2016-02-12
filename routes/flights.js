2376943c6ad72d681ae028c86d3bfb5997e55c27
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-12 10:24:46 -0700

    modified route and data structure for chart2

--- routes/flights.js
+++ routes/flights.js
@@ -28,32 +28,50 @@ router.get('/user/:userID', function(req, res){
 })
 
 router.post('/', function(req, res){
+  console.log(req.body);
   knex('flights').insert({
-    user_id: request.body.user_id,
-    flight_date: request.body.flight_date,
-    purchase_date: request.body.purchase_date,
-    flight_number: request.body.flight_number,
-    price_paid: request.body.price_paid,
-    purchase_location: request.body.purchase_location,
-    departure_airport_id: request.body.departure_airport_id,
-    arrival_airport_id: request.body.arrival_airport_id,
-    airline_id: request.body.airline_id
+    user_id: req.body.user_id,
+    flight_date: req.body.flight_date,
+    purchase_date: req.body.purchase_date,
+    flight_number: (req.body.flight_number ? parseInt(req.body.flight_number) : null),
+    price_paid: parseInt(req.body.price_paid),
+    purchase_location: (req.body.purchase_location ? req.body.purchase_location : null),
+    departure_airport_id: parseInt(req.body.departure_airport_id),
+    arrival_airport_id: parseInt(req.body.arrival_airport_id),
+    airline_id: (req.body.airline_id ? parseInt(req.body.airline_id) : null),
+    suspect: req.body.suspect
   })
+  .then(function(result){
+    console.log(result);
+    res.json(result);
+  })
+  .catch(function(error) {
+    console.error(error);
+    res.json(error);
+  });
 })
 
 router.put('/:id', function(req, res){
   knex('flights').where('id', req.params.id)
   .update({
-    flight_date: request.body.flight_date,
-    purchase_date: request.body.purchase_date,
-    flight_number: request.body.flight_number,
-    price_paid: request.body.price_paid,
-    purchase_location: request.body.purchase_location,
-    departure_airport_id: request.body.departure_airport_id,
-    arrival_airport_id: request.body.arrival_airport_id,
-    airline_id: request.body.airline_id,
-    suspect: request.body.suspect
+    flight_date: req.body.flight_date,
+    purchase_date: req.body.purchase_date,
+    flight_number: req.body.flight_number,
+    price_paid: req.body.price_paid,
+    purchase_location: req.body.purchase_location,
+    departure_airport_id: req.body.departure_airport_id,
+    arrival_airport_id: req.body.arrival_airport_id,
+    airline_id: req.body.airline_id,
+    suspect: req.body.suspect
   })
+  .then(function(result){
+    console.log(result);
+    res.json(result);
+  })
+  .catch(function(error) {
+    console.error(error);
+    res.json(error);
+  });
 })
 
 router.post('/dashboard', function(req, res){
@@ -80,43 +98,81 @@ router.post('/dashboard', function(req, res){
   flights.then(function(flights){
     flights.forEach((flight)=>{
       dashboard.chart_data.push(flight.price_paid);
-      dashboard.row_data = flights;
     })
+    dashboard.row_data = flights;
     res.json(dashboard);
   })
 })
 
 module.exports = router;
 
-// var propertiesChart2 = [
-//   "departure_airport_id",
-//   "arrival_airport_id"
-// ];
-//
-// var propertiesChart3 = [
-//   "airline_id",
-//   "departure_airport_id",
-//   "arrival_airport_id",
-//   "flight_date"
-// ]
+router.post('/dashboard/chart2', function(req, res){
+  var dashboard = {
+    chart_data: [],
+    row_data: []
+  };
 
-// var flightsChart2 = knex('flights');
-// flightsChart2.where("id", ">", 0);
-// propertiesChart2.forEach(function(property){
-//   if(req.body.hasOwnProperty(property) && req.body[property] != null
-//     && req.body[property] != 'undefined') {
-//     flightsChart2.andWhere(property, req.body[property]);
-//   }
-// })
+  var properties = [
+    "departure_airport_id",
+    "arrival_airport_id"
+  ];
+
+  var flights = knex('flights');
+  flights.where("id", ">", 0);
+  properties.forEach(function(property){
+    if(req.body.hasOwnProperty(property) && req.body[property] != null
+      && req.body[property] != 'undefined') {
+      flights.andWhere(property, req.body[property]);
+    }
+  })
+
+  // TODO: if departure_airport_id
+  flights.then(function(flights){
+    flights.forEach((flight)=>{
+      // if dashboard.chart02_data (array) has object with property label
+      // matching arrival_airport_id increment its counter
+      // else create an object with property label and value arrival_airport_id
+      // and property value should be set to 1
+      // pos = myArray.map(function(e) { return e.hello; }).indexOf('stevie');
+      var pos = dashboard.chart_data.map((e)=>{return e.label;})
+        .indexOf(flight.arrival_airport_id)
+      if (pos >= 0){
+        dashboard.chart_data[pos].value += 1;
+      } else {
+        dashboard.chart_data.push({
+          label: flight.arrival_airport_id,
+          value: 1
+        })
+      }
+    })
+    dashboard.row_data = flights;
+    res.json(dashboard);
+  })
+})
+
+router.post('/dashboard/chart3', function(req,res){
+  var properties = [
+    "airline_id",
+    "departure_airport_id",
+    "arrival_airport_id",
+    "flight_date"
+  ];
+
+  var flights = knex('flights');
+  flights.where("id", ">", 0);
+  properties.forEach(function(property){
+    if(req.body.hasOwnProperty(property) && req.body[property] != null
+      && req.body[property] != 'undefined') {
+      flights.andWhere(property, req.body[property]);
+    }
+  })
+})
+
 //
-// var flightsCharts3 = knex('flights');
-// flightsChart3.where("id", ">", 0);
-// propertiesChart3.forEach(function(property){
-//   if(req.body.hasOwnProperty(property) && req.body[property] != null
-//     && req.body[property] != 'undefined') {
-//     flightsChart3.andWhere(property, req.body[property]);
-//   }
-// })
+
+
+
+
 
 
 // Coerce the query builder into a promise
@@ -133,29 +189,8 @@ module.exports = router;
 // flights.then(function(flights){
 //   flightsChart2.then(function(flights2){
 //     flightsChart3.then(function(flights3){
-//       // TODO: if any of the 4 req.body vars
-//       flights.forEach((flight)=>{
-//         dashboard.chart01_data.push(flight.price_paid);
-//         dashboard.row_data = flights;
-//       })
-//       // TODO: if departure_airport_id
-//       flights2.forEach((flight)=>{
-//         // if dashboard.chart02_data (array) has object with property label
-//         // matching arrival_airport_id increment its counter
-//         // else create an object with property label and value arrival_airport_id
-//         // and property value should be set to 1
-//         // pos = myArray.map(function(e) { return e.hello; }).indexOf('stevie');
-//         var pos = dashboard.chart02_data.map((e)=>{return e.label;})
-//           .indexOf(flight.arrival_airport_id)
-//         if (pos >= 0){
-//           dashboard.chart02_data[pos].value += 1;
-//         } else {
-//           dashboard.chart02_data.push({
-//             label: flight.arrival_airport_id,
-//             value: 1
-//           })
-//         }
-//       })
+
+//
 //       // TODO: if airline_id && departure_airport_id && arrival_airport_id && flight_date
 //       flights3.forEach((flight)=>{
 //         // line graph of prices +/- 7 days of flight_date
