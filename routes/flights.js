6f238058e4f0199f22d7d8284afa56ba7dd6010f
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-15 11:16:55 -0700

    test merge to avoid conflicts test

--- routes/flights.js
+++ routes/flights.js
@@ -104,7 +104,6 @@ router.post('/dashboard', function(req, res){
   })
 })
 
-module.exports = router;
 
 router.post('/dashboard/chart2', function(req, res){
   var dashboard = {
@@ -165,3 +164,5 @@ router.post('/dashboard/chart3', function(req,res){
     res.json("work in progress")
   })
 })
+
+module.exports = router;
